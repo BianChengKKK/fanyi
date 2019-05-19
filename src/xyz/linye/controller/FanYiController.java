236539ad17bccd2f50b9aa77569cca8df1c4d21f
linyunze <289373274@qq.com>
2019-05-19 10:56:34 +0800

    翻译完成

--- src/xyz/linye/controller/FanYiController.java
+++ src/xyz/linye/controller/FanYiController.java
@@ -1,10 +1,8 @@
 package xyz.linye.controller;
 
-import org.junit.Test;
 import xyz.linye.utils.TransApi;
 
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -17,7 +15,6 @@ public class FanYiController extends HttpServlet {
     private static final String APP_ID = "20190515000297947";
     private static final String SECURITY_KEY = "dd2xiOxA0s7txdqUc6vL";
 
-    @Test
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String fromValue = request.getParameter("fromValue");
 
